af1071fef284f185cda75def7b883734b6abab8f
obasco <oobayemi3@gmail.com>
2026-01-07 23:54:34 +0100

    GWC

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "./assets/logo.png";
+import logo from "../src/assets/logo.png";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
